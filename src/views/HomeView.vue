a9413a19638b870f193ee88c2eeb2d226fce4286
yujingzhou <johnjzyu@dshealthcareservices.com>
2023-11-25 17:16:55 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <NesVue
+      url="https://taiyuuki.github.io/nes-vue/Super Mario Bros (JU).nes"
+    />
+  </div>
+</template>
+
+<script>
+import { NesVue } from "nes-vue";
+export default {
+  name: "HomeView",
+  components: {
+    NesVue,
+  },
+};
+</script>
